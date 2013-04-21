5b28399d1f79c70beebe1fed10e308632418e5f3
Martin Walsh <mwalsh@leapfrogonline.com>
2013-04-21 13:34:14 -0500

    speed up builds during testing, by no cleaning up chroot

--- lakehead/lakehead.py
+++ lakehead/lakehead.py
@@ -63,7 +63,7 @@ def build(opts):
                 config.sources = sources
                 mock_cmd = ('/usr/bin/mock --configdir=%(configdir)s -r mock'
                             ' --buildsrpm --spec=%(name)s.spec'
-                            ' --resultdir=%(results)s'
+                            ' --resultdir=%(results)s --no-clean'
                             ' --sources=%(sources)s' % config).split()
                 mock = Popen(mock_cmd)
                 mock.communicate()
